8e77311eac1b6ce7b8d7d3e039d82a85df84251b
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-10 16:31:36 +0530

    added guide

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    <p>Home</p>
+  <div class="wrapper">
+
+    <app-guide id="guide"></app-guide>
+    
   </div>
 </template>
 
 <script>
+import guide from "./guide.vue"
 export default {
   name: "home",
+  components: {
+    appGuide: guide
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+#guide{
+  z-index: 1000;
+  position: relative;
+}
+
+
+
+</style>
