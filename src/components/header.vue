d9bc69336b2a52bd5e42dd2b2ebefb4bd9e15306
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-10 13:31:49 +0530

    added header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,21 @@
 <template>
-    <h1>Header</h1>
+    <div id=header>
+  <b-navbar toggleable="lg" type="dark">
+    <b-navbar-brand href="#">Home</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+       <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-navbar-nav>
+        <b-nav-item href="#">About Us</b-nav-item>
+        <b-nav-item href="#">Contact Us</b-nav-item>
+      </b-navbar-nav>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
 </template>
 
 <script>
@@ -10,5 +26,9 @@ export default {
 
 
 <style scoped>
-
+#header{
+    background-color: #228be6;
+    font-family: 'Comic Neue', cursive;
+    font-size: 2vw;
+}
 </style>
