325fdba1e8e7342b05a7b60eb5688cccf045244a
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-06-10 13:46:52 +0530

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <app-header></app-header>
+    <app-footer></app-footer>
   </div>
+
 </template>
 
 <script>
 import header from "./components/header.vue";
+import footer from "./components/footer";
 
 export default {
   name: "App",
   components: {
     appHeader: header,
+    appFooter: footer
   },
 };
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="footer" id="footer">
+        <a href="https://github.com/codetheorem/Grader" id="logo"><i class="fa fa-github" aria-hidden="true"></i></a>
+        <p>Developed by <a href="https://github.com/codetheorem">Hrishikesh</a> | <a href="https://github.com/vinayaksh42">Vinayak</a></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "footer"
+    }
+</script>
+
+<style scoped>
+    #logo{
+        font-size: 30px;
+    }
+    a{
+        color: white;
+    }
+    .footer {
+        font-family: 'Comic Neue', cursive;
+        font-size: large;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #228be6;
+        color: white;
+        text-align: center;
+    }
+    p{
+        font-size: larger;
+    }
+</style>
