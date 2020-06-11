a1d86e822bd98ec21983122021d2e903c8ea7156
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-06-11 16:12:04 +0530

    credit

--- src/components/home.vue
+++ src/components/home.vue
@@ -103,6 +103,7 @@ export default {
   data() {
     return {
       cgpa: 0,
+      count: 0,
       result: false,
       selected: null,
       options: [
@@ -137,9 +138,12 @@ export default {
     },
     submit() {
       this.cgpa = 0;
+      this.count = 0;
       for (let key in this.course) {
-        this.cgpa += this.course[key].grade;
+        this.cgpa += this.course[key].grade * this.course[key].credit;
+        this.count+=this.course[key].credit;
       }
+      this.cgpa/=this.count;
       this.result = !this.result;
     },
   },
