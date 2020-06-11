ba8a09bb8b66a4b2058f265da05afd3cdc2b058f
codetheorem <hrishikeshagarwalv@gmail.com>
2020-06-11 17:02:22 +0530

    cse

--- src/courses.js
+++ src/courses.js
@@ -3,9 +3,20 @@ export const courses = {
     return {
       options1: [
         { value: null, text: "Please select an option" },
-        { value: 2, text: "ENG-MATHS 1" },
-        { value: 3, text: "c programming" },
+        { value: 3, text: "ENG-MATHS 1" },
+        { value: 4, text: "c programming" },
         { value: 4, text: "python" },
+        { value: 3, text: "DHV" },
+        { value: 3, text: "ENG-maths 2" },
+        { value: 4, text: "Data Structure using C" },
+        { value: 5, text: "Digital Electronics" },
+        { value: 4, text: "Basic Electronics" },
+        { value: 1, text: "workshop" },
+        { value: 1, text: "Engineering Graphics" },
+        { value: 3, text: "Professional Communication" },
+        { value: 4, text: "Engineering Physics" },
+        { value: 1, text: "Intro to computer science Engineering" },
+        { value: 1, text: "Language Lab" },
       ],
       options2: [
         { value: null, text: "Please select an option" },
